0b5f7d8523b005407ceb0477755a7d5356e90dcc
Maysa853 <Maysa.vicente@etec.sp.gov.br>
2025-03-06 18:15:30 -0300

    Aula 0603- modulos internos e externos

---
+++ Aula 3/calculadora.js
@@ -0,0 +1,16 @@
+module.exports={
+    calcula(a, b, op){
+        if(op === "+"){
+            return(a+b)
+        }else if(op === "-"){
+            return(a-b)
+        }else if(op === "*"){
+            return(a*b)
+        }else if(op === "/"){
+            return(a/b)
+        }else{
+            return(`${op} não existe.`)
+        }
+    }
+}
+ 
